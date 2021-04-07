955812dfe46a18aa88947023629515a6910ad954
Evelyn Tingqi Fan <tfan1@inside.artcenter.edu>
2021-04-07 18:03:59 +0800

    1

---
+++ finalgame/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb; 
+    Vector2 movement2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement2.x = Input.GetAxisRaw("Horizontal");
+        movement2.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement2 * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ finalgame/Assets/movementb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.L)) 
+        {
+            gameObject.transform.position += new Vector3(0.1f,0,0);
+        }
+
+        if (Input.GetKey (KeyCode.J)) 
+        {
+            gameObject.transform.position += new Vector3(-0.1f,0,0);
+        }
+  
+        if (Input.GetKey (KeyCode.I)) 
+        {
+            gameObject.transform.position += new Vector3(0,0.1f,0);
+        }
+
+        if (Input.GetKey (KeyCode.K)) 
+        {
+            gameObject.transform.position += new Vector3(0,-0.1f,0);
+        }
+ 
+    }
+}
